933fc511eb66313d35753913087f9b76613255b0
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-06-04 15:51:25 -0600

    Se inicializaron atributos en el constructor

--- FinalTest/src/finaltest/DepartmentsArrays.java
+++ FinalTest/src/finaltest/DepartmentsArrays.java
@@ -6,4 +6,12 @@ public class DepartmentsArrays {
     private String header;
     private DepartmentsArrays[] departments;
     private int countDepartment;
+    
+    public DepartmentsArrays(String departmentName, int numberMunicipalities, String header){
+        this.departmentName = departmentName;
+        this.numberMunicipalities = numberMunicipalities;
+        this.header = header;
+        departments = new DepartmentsArrays[22];
+        countDepartment = 0;
+    }
 }
