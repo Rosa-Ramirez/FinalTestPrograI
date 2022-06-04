5266a65cc85cf76bc6c7a023d6b671b7d79d2677
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-06-04 16:50:44 -0600

    Se crea un metodo para registrar los departamentos con Arrays

--- FinalTest/src/finaltest/DepartmentsArrays.java
+++ FinalTest/src/finaltest/DepartmentsArrays.java
@@ -1,5 +1,8 @@
 
 package finaltest;
+
+import java.util.Scanner;
+
 public class DepartmentsArrays {
     private String departmentName;
     private int numberMunicipalities;
@@ -13,5 +16,24 @@ public class DepartmentsArrays {
         this.header = header;
         departments = new DepartmentsArrays[22];
         countDepartment = 0;
+    }
+    
+    Scanner sc = new Scanner (System.in);
+    
+    public void registerDepartments(){
+        System.out.println("-------------Registrar Departamento-------------");
+        
+        System.out.println("--> Ingrese el nombre: ");
+        departmentName = sc.nextLine();
+        
+        System.out.println("--> Ingrese la cantidad de Municipios");
+        numberMunicipalities = sc.nextInt();
+        
+        System.out.println("--> Ingrese la cabecera: ");
+        header = sc.nextLine();
+        
+        departments[countDepartment] = new DepartmentsArrays(departmentName, numberMunicipalities, header);
+        countDepartment++;
+        System.out.println("-------------Departamento Registrado Correctamente-------------");
     }
 }
