2e277020c68e134f59e52af2b097bc823ee8be52
Rosa-Ramirez <rramireze5@miumg.edu.gt>
2022-06-04 15:44:46 -0600

    Se declararon atributos de la clase y se creó un vector para departamentos

---
+++ FinalTest/src/finaltest/DepartmentsArrays.java
@@ -0,0 +1,9 @@
+
+package finaltest;
+public class DepartmentsArrays {
+    private String departmentName;
+    private int numberMunicipalities;
+    private String header;
+    private DepartmentsArrays[] departments;
+    private int countDepartment;
+}
